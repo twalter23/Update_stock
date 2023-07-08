fa00e1176f734003ae71a8b4d4ac87848d7a2d67
Twalter <t.walter@laas.fr>
2023-07-08 09:19:29 +0200

    dev

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import csv
 import os
+import random
+import openpyxl
+
+def read_xlsx_file(file_path):
+    workbook = openpyxl.load_workbook(file_path)
+    sheet = workbook.active
+    data = []
+    for row in sheet.iter_rows(values_only=True):
+        data.append(row)
+    return data
 
 def read_csv_file(file_path):
     data = []
@@ -12,27 +22,62 @@ def read_csv_file(file_path):
                 data.append([manf, quantity])
     return data
 
-def write_csv_file(file_path, data):
+def write_csv_file(file_path, BOM, Number_of_boards):
+
     directory = os.path.dirname(file_path)
     output_file = os.path.join(directory, 'BOM_FOUND.csv')
+
     with open(output_file, 'w', newline='') as file:
         writer = csv.writer(file)
-        header = ['manf#', 'Quantity']
+        header = ['manf#', 'Quantity', 'Found']
         writer.writerow(list(header))
-        writer.writerows(data)
+
+        for row in BOM:
+            manf = row[0]
+            quantity = int(row[1]) * int(Number_of_boards)
+            found = row[2]
+            writer.writerow([quantity, manf, found])  # Write the updated row
+
     print("The 'BOM_FOUND.csv' file has been created.")
 
+def write_xlsx_file(file_path, data):
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    for row in data:
+        sheet.append(row)
+    workbook.save(file_path)
+    print(f"The '{file_path}' file has been created.")
+
+
 def main():
-    # Prompt the user to enter the local path of the CSV file
-    file_path = input("Enter the local path of the CSV file: ")
-    file_path = file_path.strip("& '")
+    # Prompt
+    BOM_CSV_Path = input("Enter the local path of the BOM CSV file: ")
+    BOM_CSV_Path = BOM_CSV_Path.strip("& '")
+
+    STOCK_XLSX_Path = input("Enter the local path of the STOCK XLSX file: ")
+    STOCK_XLSX_Path = STOCK_XLSX_Path.strip("& '")
 
     # Read the CSV file and store the content of 'quantity' and 'manf' columns in a 2D array
-    csv_data = read_csv_file(file_path)
+    BOM_DATA = read_csv_file(BOM_CSV_Path)
 
-    # Write the 2D array into a CSV file named 'BOM_FOUND' at the same path as the opened CSV file
-    write_csv_file(file_path, csv_data)
+    # Read the XLSX file and store its content in an array
+    STOCK_DATA = read_xlsx_file(STOCK_XLSX_Path)
 
+    for row in STOCK_DATA:
+        if isinstance(row[0], int):
+            row[0] -= 1
+
+    directory_STOCK = os.path.dirname(STOCK_XLSX_Path)
+    output_file_STOCK = os.path.join(directory_STOCK, 'STOCK_UPDATED.xlsx')
+    write_xlsx_file(output_file_STOCK, STOCK_DATA)
+
+    # Print the content of the XLSX file
+    for row in STOCK_DATA:
+        print(row)
+
+    # # Write the 2D array into a CSV file named 'BOM_FOUND' at the same path as the opened CSV file
+    # Number_of_boards = input("Enter the number of fabricated boards: ")
+    # write_csv_file(BOM_CSV_Path, BOM_DATA, Number_of_boards)
 
 #**********************************************#
 #                    main                      #
